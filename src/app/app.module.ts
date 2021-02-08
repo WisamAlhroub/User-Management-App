8cc0f422947435e2171470b4f8f7480e3c9badae
Wisam Alhroub <wisalhroub@gamail.com>
2021-02-08 22:46:29 +0200

    Added Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { UsersPageComponent } from './Components/users-page/users-page.component';
+import { AddEditComponent } from './Components/add-edit/add-edit.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    UsersPageComponent,
+    AddEditComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
